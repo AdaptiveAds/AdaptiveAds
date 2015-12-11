3cd1576ea3be047cbeec20c0b1c1d26dbbe55f1d
Josh Preece <looterwar@gmail.com>
2015-12-11 23:13:58 +0000

    Added auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('advert/{id}', 'AdvertController@show');
+Route::get('dashboard', ['middleware' => 'auth', function() {
+    echo 'Welcome ' . Auth::user()->username;
+}]);
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('page/{id}', 'PageController@show');
-
-Route::get('serve/{screenID}', 'DisplayController@Show');
