616ab63dcf1daad87a003a5d25161f46fbe1f492
Josh Preece <looterwar@gmail.com>
2016-01-01 19:41:40 +0000

    Initial commit of playlist Model and Controller

---
+++ app/Playlist.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Playlist extends Model
+{
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'playlist';
+  public $timestamps = false;
+
+
+}
