282260568a1db6e1653dec37c48a379d4891b988
Josh Preece <looterwar@gmail.com>
2016-01-01 14:52:21 +0000

    Interface update
    - Includes new fields that were missing
    - Includes routes for function
    - Allows the page to be used for CRUD

--- resources/views/pages/pageeditor.blade.php
+++ resources/views/pages/pageeditor.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h3>Page Editor</h3>
-<h3>Page Title</h3>
+<h3>Title: {{ $pageData->page_data_name or 'New Page'}}</h3>
 
 <div id="left" class="landscape">
 	<div class="pagecontainer">
@@ -25,31 +25,48 @@
 </div>
 <div id="right">
 
-	<!-- PHP Driven self updating ?? -->
-	<form name="pageEditor" action="index_submit" method="get" accept-charset="utf-8">
+	<!-- PHP Driven self updating ??
+	<form name="pageEditor" action="index_submit" method="get" accept-charset="utf-8"> -->
 		<ul>
+		@if (isset($pageData))
+			{!! Form::open(['route' => ['dashboard.advert.{adID}.page.update', $page->id], 'method' => 'PUT']) !!}
+		@else
+			{!! Form::open(['route' => ['dashboard.advert.{adID}.page.store',  $page->advert_id], 'method' => 'POST']) !!}
+		@endif
 			<li id="orientation">
 				<button class="btn-orientationHor" type="button">Landscape</button>
 				<button class="btn-orientationVert" type="button">Portrait</button>
 				<div class="clear"></div>
 			</li>
 			<li>
-				<label>Image:</label>
-				<input type="text" name="image"><br>
-				<input class="submit" type="submit" value="Submit">
+				<label>Title</label>
+				<input type="text" placeholder="Page Name...." name="pageName"/>
 			</li>
-			<li><textarea title="content" type="text" name="content" placeholder="Image Meta" required></textarea></li>
 			<li>
-
-				<label>Video:</label>
-				<input type="text" name="video"><br>
-				<input class="submit" type="submit" value="Submit">
+				<label>Image:</label>
+				<input type="text" name="pageImage" placeholder="Image path..."><br>
 			</li>
-			<li><textarea title="content" type="text" name="content" placeholder="Video Meta" required></textarea></li>
-			<li><textarea title="content" type="text" name="content" placeholder="Enter Content" required></textarea></li>
+			<li><textarea title="content" type="text" name="imageMeta" placeholder="Image Meta..." required></textarea></li>
+			<li>
+				<label>Video:</label>
+				<input type="text" name="pageVideo" placeholder="Video path..."><br>
+			</li>
+			<li><textarea title="content" type="text" name="videoMeta" placeholder="Video Meta..." required></textarea></li>
+			<li>
+				<label>Page Index: </label>
+				<input type="number" name="pageIndex" value="0"/>
+			</li>
+			<li>
+				<label>Content</label>
+				<textarea title="content" type="text" name="pageContent" placeholder="Enter Content..." required></textarea>
+			</li>
+			<li><button type"submit" class"submit">Save</button></li>
+			{!! Form::close() !!}
+			{!! Form::open(['route' => ['dashboard.advert.{adID}.page.destroy', $page->id], 'method' => 'DELETE']) !!}
+			<li><button type="submit" class="submit">Delete</button></li>
+			{!! Form::close() !!}
 			<!-- ensures form fills parent div w3c validation compliant -->
 			<div class="clear"></div>
 		</ul>
-	</form>
 </div>
 @endsection
