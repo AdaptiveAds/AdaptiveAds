692c55db984ccea1c653359e37c60c5a074cf53a
Josh Preece <looterwar@gmail.com>
2016-01-01 14:57:06 +0000

    Removes timestamps

--- app/PageData.php
+++ app/PageData.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PageData extends Model
 {
     protected $table = 'page_data';
+    public $timestamps = false;
 
     public function Page() {
       return $this->belongsTo(Page::class);
