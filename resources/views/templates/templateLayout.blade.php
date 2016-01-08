1f963dff0d7aa445a24770f42f2f9b849c63f679
bootfairy <whelan.kane@gmail.com>
2016-01-08 19:21:15 +0000

    Template(s) Designs
    CSS + HTML

--- resources/views/templates/templateLayout.blade.php
+++ resources/views/templates/templateLayout.blade.php
@@ -1,3 +1,4 @@
+<!-- -->
 @extends('templateDefault')
 
 @section('content')
@@ -9,32 +10,32 @@
 		<h1>Template</h1>
 	</div>
 
-    <div class="row1">     
+    <div class="row1">
 
-    	<div class="quad">             
-    		<img src="" title="" alt=""/> 
-    	</div>  
+    	<div class="quad">
+    		<img src="" title="" alt=""/>
+    	</div>
 
 	    <div class="quad">
 	    	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
 				tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
 				ultricies mi vitae est. Mauris placerat eleifend leo.
 			</p>
-		</div> 
+		</div>
 	</div>
 
-	<div class="row2">     
+	<div class="row2">
 
-    	<div class="quad">             
-    		<img src="" title="" alt=""/> 
-    	</div>  
+    	<div class="quad">
+    		<img src="" title="" alt=""/>
+    	</div>
 
 	    <div class="quad">
 	    	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
 				tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
 				ultricies mi vitae est. Mauris placerat eleifend leo.
 			</p>
-		</div> 
+		</div>
 	</div>
 
 
