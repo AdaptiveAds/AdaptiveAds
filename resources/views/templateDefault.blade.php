174508c1b58a07748b2e03988d0e4d3fb37caf8c
bootfairy <whelan.kane@gmail.com>
2016-01-08 19:11:04 +0000

    Template(s) related files and css
    Some code in scss files contains CSS as Jack will be converting it to
    scss.

---
+++ resources/views/templateDefault.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>"{{ $pageName }}"</title>
+	<link rel="stylesheet" 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" 	href="{{ URL::asset('css/template.css') }}" type="text/css">
+</head>
+
+<body id="{{ $pageName }}">
+<div id="wrapper">
+		
+	<div id="content">
+    	@yield('content')
+    	<div class="clear"></div>
+	    <!-- close content -->
+  	</div>
+</body>
+</html>
