54729f69bf8ba28551cdb1e78f4f22490e528fc6
bootfairy <whelan.kane@gmail.com>
2016-01-08 19:17:12 +0000

    Updated Template Layouts

---
+++ resources/views/templates/templateLayout.blade.php
@@ -0,0 +1,45 @@
+@extends('templateDefault')
+
+@section('content')
+
+<div id="ad_container">
+
+	<div id="header">
+
+		<h1>Template</h1>
+	</div>
+
+    <div class="row1">     
+
+    	<div class="quad">             
+    		<img src="" title="" alt=""/> 
+    	</div>  
+
+	    <div class="quad">
+	    	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+				tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
+				ultricies mi vitae est. Mauris placerat eleifend leo.
+			</p>
+		</div> 
+	</div>
+
+	<div class="row2">     
+
+    	<div class="quad">             
+    		<img src="" title="" alt=""/> 
+    	</div>  
+
+	    <div class="quad">
+	    	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+				tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
+				ultricies mi vitae est. Mauris placerat eleifend leo.
+			</p>
+		</div> 
+	</div>
+
+
+</div>
+
+<div id="footer"></div>
+
+@endsection
