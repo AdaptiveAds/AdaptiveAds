6e5217fe063c1f48f5969b2ff41ba5447eb7fa6a
Josh Preece <looterwar@gmail.com>
2016-01-12 13:58:05 +0000

    Updated to match various states

--- resources/views/objects/advertItem.blade.php
+++ resources/views/objects/advertItem.blade.php
@@ -1,7 +1,14 @@
 <ul class="advertList">
 @if (isset($adverts))
   @foreach($adverts as $advert)
-    <li class="advertItem"><a href="{{ URL::route('dashboard.advert.show', $advert->id) }}">{{ $advert->advert_name }}</a></li>
+    <!-- Only display select if we're adding to a playlist -->
+    @if (isset($selectedPlaylist))
+      <li class="advertItem"><a href="{{ URL::route('dashboard.playlist.add', [$selectedPlaylist, $advert->id]) }}">{{ $advert->advert_name }}</a></li>
+    @elseif (isset($deleteMode))
+      <li class="advertItem"><a href="{{ URL::route('dashboard.playlist.remove', [$playlist->id, $advert->id]) }}">{{ $advert->advert_name }}</a></li>
+    @else
+      <li class="advertItem"><a href="{{ URL::route('dashboard.advert.show', $advert->id) }}">{{ $advert->advert_name }}</a></li>
+    @endif
   @endforeach
 @endif
 </ul>
