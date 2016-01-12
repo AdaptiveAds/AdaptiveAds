84f9a154b06d91996a474729f37ebd6d2b07c626
Josh Preece <looterwar@gmail.com>
2016-01-12 17:31:32 +0000

    Outer page structure for displaying templates/pages

--- resources/views/templateDefault.blade.php
+++ resources/views/templateDefault.blade.php
@@ -2,16 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-	<title>"{{ $pageName }}"</title>
+	<title>Playlist: {{ $playlist->playlist_name }}</title>
 	<link rel="stylesheet" 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" 	href="{{ URL::asset('css/template.css') }}" type="text/css">
 </head>
 
-<body id="{{ $pageName }}">
+<body id="">
 <div id="wrapper">
-		
+
 	<div id="content">
-    	@yield('content')
+    	@include('templateStructure')
     	<div class="clear"></div>
 	    <!-- close content -->
   	</div>
