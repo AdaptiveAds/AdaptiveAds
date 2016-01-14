d5a6f6d6d6f1a16f413d485f66cfb61b6ecedeeb
Josh Preece <looterwar@gmail.com>
2016-01-14 10:38:45 +0000

    Updated

--- resources/views/templateDefault.blade.php
+++ resources/views/templateDefault.blade.php
@@ -2,16 +2,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-	<title>Playlist: {{ $playlist->playlist_name }}</title>
+	<title>Playlist: {{ $playlist[0]->playlist_name }} </title>
 	<link rel="stylesheet" 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" 	href="{{ URL::asset('css/template.css') }}" type="text/css">
+	<script src="{{ URL::asset('js/jquery-2.1.4.js') }}"></script>
+	<script src="{{ URL::asset('js/pages.js') }}"></script>
 </head>
 
 <body id="">
 <div id="wrapper">
 
 	<div id="content">
-    	@include('templateStructure')
+    	@yield('content')
     	<div class="clear"></div>
 	    <!-- close content -->
   	</div>
