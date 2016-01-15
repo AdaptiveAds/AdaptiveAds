df877e931018ad6d19f87eaac9f2893d66c54112
Josh Preece <looterwar@gmail.com>
2016-01-15 00:18:54 +0000

    Updates

--- app/HorizontalTemplate.php
+++ app/HorizontalTemplate.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class HorizontalTemplate extends Model
 {
-    //
+    protected $table = 'horizontal_template';
+    public $timestamps = false;
+
+    public function Template()
+    {
+        return $this->hasOne(Template::class, 'id', 'template_id');
+    }
 }
---
+++ app/Transition.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transition extends Model
+{
+    protected $table = 'transition';
+    public $timestamps = false;
+}
