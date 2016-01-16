a551a012b3efc983d9b9d3a5ed671e2f4709c36b
Josh Preece <joshpreece@outlook.com>
2016-01-16 22:30:46 +0000

    Refactored

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,18 +15,19 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(TemplateTableSeeder::class);
-        $this->call(AdvertTableSeeder::class);
         $this->call(PageDataTableSeeder::class);
-        $this->call(PageTableSeeder::class);
         $this->call(DisplayScheduleTableSeeder::class);
-        $this->call(PlaylistTableSeeder::class);
-        $this->call(AdvertPlaylistTableSeeder::class);
-        $this->call(LocationTableSeeder::class);
         $this->call(SkinTableSeeder::class);
+        $this->call(LocationTableSeeder::class);
+        $this->call(PrivilageTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        // TODO display timing
         $this->call(DepartmentTableSeeder::class);
         $this->call(ScreenTableSeeder::class);
-        $this->call(UserTableSeeder::class);
-        $this->call(PrivilageTableSeeder::class);
+        $this->call(AdvertTableSeeder::class);
+        $this->call(PlaylistTableSeeder::class);
+        $this->call(PageTableSeeder::class);
+        $this->call(AdvertPlaylistTableSeeder::class);
         $this->call(DepartmentUserTableSeeder::class);
 
         Model::reguard();
