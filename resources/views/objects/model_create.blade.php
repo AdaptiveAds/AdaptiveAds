bf8b9b1d2a6257dcbfea575513c51020b6dce0c1
Josh Preece <joshpreece@outlook.com>
2016-01-23 18:31:22 +0000

    Styling

--- resources/views/objects/model_create.blade.php
+++ resources/views/objects/model_create.blade.php
@@ -3,14 +3,23 @@
     <a href="#close" class="close">X</a>
     <h4>Create new {{$object or 'object'}}</h4>
     {!! Form::open(['url' => 'dashboard', 'method' => 'GET']) !!}
-      <input type="text" name="txt{{$object or 'object'}}Name" placeholder="Name..."/>
-      <select name="drpDepartments">
-        @foreach ($allowed_departments as $department)
-          <option value="{{$department->id}}">{{$department->name}}</option>
-        @endforeach
-      </select>
-      <br/>
-      <input type="submit" name="btnCreate{{$object or 'object'}}" value="Create"/>
+      <div class="modalDialogLeft">
+        <label>{{$object or 'object'}} Name</label>
+        <br/>
+        <input type="text" name="txt{{$object or 'object'}}Name" placeholder="Name..."/>
+      </div>
+
+      <div class="modalDialogLeft">
+        <label>Assign Department</label>
+        <br/>
+        <select name="drpDepartments">
+          @foreach ($allowed_departments as $department)
+            <option value="{{$department->id}}">{{$department->name}}</option>
+          @endforeach
+        </select>
+      </div>
+      <div class="clear"></div>
+      <input type="submit" class="submit" name="btnCreate{{$object or 'object'}}" value="Create"/>
     {!! Form::close() !!}
   </div>
 </div>
