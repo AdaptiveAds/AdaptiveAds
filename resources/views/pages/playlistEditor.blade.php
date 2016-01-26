328e65a3bb3826b83c396359eb8589e439831055
Josh Preece <joshpreece@outlook.com>
2016-01-26 17:00:12 +0000

    Removed redundant forms

--- resources/views/pages/playlistEditor.blade.php
+++ resources/views/pages/playlistEditor.blade.php
@@ -30,14 +30,11 @@
 </div>
 <div id="right">
 
-	<!-- PHP Driven self updating ?? -->
-	<form name="advertlist" action="index_submit" method="get" accept-charset="utf-8">
-		<ul>
-			<li><button type="button">Up</button></li>
-			<li><button type="button">Down</button></li>
-			<!-- ensures form fills parent div w3c validation compliant -->
-			<div class="clear"></div>
-		</ul>
-	</form>
+	<ul>
+		<li><button id="btnUp" type="button">Up</button></li>
+		<li><button id="btnDown" type="button">Down</button></li>
+		<!-- ensures form fills parent div w3c validation compliant -->
+		<div class="clear"></div>
+	</ul>
 </div>
 @endsection
