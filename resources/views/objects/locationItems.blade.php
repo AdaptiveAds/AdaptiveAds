6b24ea363ba9373acc3e1fe03e9f7835ec354ec2
Josh Preece <joshpreece@outlook.com>
2016-01-27 17:45:01 +0000

    Properly check for no locations

--- resources/views/objects/locationItems.blade.php
+++ resources/views/objects/locationItems.blade.php
@@ -1,15 +1,17 @@
 <ul>
   @if (isset($locations))
-    @foreach($locations as $location)
+    @if ($locations->count() > 0)
+      @foreach($locations as $location)
+        <li>
+          <label for="name">{{ $location->name or 'Location name' }}</label>
+          <button type="button">Edit</button>
+          <button type="button">Disable</button>
+        </li>
+      @endforeach
+    @else
       <li>
-        <label for="name">{{ $location->name or 'Location name' }}</label>
-        <button type="button">Edit</button>
-        <button type="button">Disable</button>
+        <h3>No locations found</h3>
       </li>
-    @endforeach
-  @else
-    <li>
-      <h3>No locations found</h3>
-    </li>
+    @endif
   @endif
 </ul>
