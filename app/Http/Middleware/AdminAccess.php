44d231444f2ccf5987128fb8fced77a61f2b77b3
Josh Preece <joshpreece@outlook.com>
2016-02-03 16:58:29 +0000

    Added AdminAccess Middleware
    - This middleware can be used on routes that only admins and supers can
    access this reduces the `allowed_deps` array to only admin `deps` the
    user has permission to modify

--- app/Http/Middleware/AdminAccess.php
+++ app/Http/Middleware/AdminAccess.php
@@ -20,10 +20,30 @@ class AdminAccess
       $user = Session::get('user');
       $super_user = Session::get('super_user');
 
+      // Get the users allowed departmentments
+      // includes both admin and user access/permission
+      $allowed_departments = Session::get('allowed_departments');
+      $admin_departments = [];
+
       // Does user have permission to access this page?
-      if ($user->getAdmin() == false && $super_user == false) {
-        return response('Unauthorized.', 401);
+      if ($super_user == false) {
+        if ($user->getAdmin() == false) {
+          return response('Unauthorized.', 401);
+        } else{
+          // Only allow those defined by the database
+          foreach ($allowed_departments as $department) {
+            if ($department->getAdmin() == true) {
+              array_push($admin_departments, $department);
+            }
+          }
+        }
+      } else {
+        $admin_departments = $allowed_departments;
       }
+
+      // Save an array of the departments that this user has
+      // admin permissions to access
+      Session::flash('allowed_departments', $admin_departments);
 
       return $next($request);
 
