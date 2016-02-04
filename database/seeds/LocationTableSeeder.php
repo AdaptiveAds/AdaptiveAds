fd811a38e0e7f3270f2d4ec8109cd67c10974f5d
Josh Preece <joshpreece@outlook.com>
2016-02-04 15:50:46 +0000

    Git why you miss files!?!?!?!

--- database/seeds/LocationTableSeeder.php
+++ database/seeds/LocationTableSeeder.php
@@ -19,7 +19,7 @@ class LocationTableSeeder extends Seeder
       //Populate table
       $table->insert([
         'name' => 'Park',
-        'department_id' => 1
+        'department_id' => 2
       ]);
     }
 }
