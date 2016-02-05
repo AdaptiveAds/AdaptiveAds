0000d74b906d8466dc87713ff4f3125b66018c5e
Josh Preece <joshpreece@outlook.com>
2016-02-05 22:01:35 +0000

    Updated

--- resources/views/objects/playlistItem.blade.php
+++ resources/views/objects/playlistItem.blade.php
@@ -1,7 +1,17 @@
 <ul class="advertList">
 @if (isset($playlists))
   @foreach($playlists as $playlist)
-    <li class="advertItem"><a href="{{ URL::to('dashboard/playlist/' . $playlist->id)}}">{{ $playlist->name }}</a></li>
+    <li class="advertItem">
+      <a href="{{ URL::to('dashboard/playlist/' . $playlist->id)}}">{{ $playlist->name }}</a>
+      <button type="submit">Edit</button>
+      @if ($playlist->deleted == 0)
+        <button type="submit">Disable</button>
+      @else
+        @if ($user->getAdmin())
+          <button type="submit">Enable</button>
+        @endif
+      @endif
+    </li>
   @endforeach
 @endif
 </ul>
