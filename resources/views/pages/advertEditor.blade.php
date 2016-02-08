dd291307069688e002fec497d290f7391bf8c1e2
Josh Preece <joshpreece@outlook.com>
2016-02-08 12:44:48 +0000

    SelectManager now generic
    Use data-selectableList and data-selectableItem to make a selectable
    list

--- resources/views/pages/advertEditor.blade.php
+++ resources/views/pages/advertEditor.blade.php
@@ -20,8 +20,8 @@
 					{!! Form::open(['route' => ['dashboard.advert.destroy', $advert->id], 'method' => 'DELETE']) !!}
 					<input type="name" name="txtPlaylistName" placeholder="Advert name...."
 								 value="{{ $searchItem or '' }}"/>
-					<button id="btnUp" type="button">Up</button>
- 					<button id="btnDown" type="button">Down</button>
+					<button id="btnUp" type="button" disabled>Up</button>
+ 					<button id="btnDown" type="button" disabled>Down</button>
 					<button type="button" name="btnNewPage" onclick="location.href='{{ URL::route('dashboard.advert.{adID}.page.create', $advert->id) }}';">+ New Page</button>
 					<button name="btnDeleteAdvert" type="submit">Delete Ad</button>
 					{!! Form::close() !!}
