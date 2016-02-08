bf6ffb199ca0b5203d908f14185bd9e1876af4fb
Josh Preece <joshpreece@outlook.com>
2016-02-08 17:00:29 +0000

    Fixed route

--- resources/views/pages/advertEditor.blade.php
+++ resources/views/pages/advertEditor.blade.php
@@ -17,14 +17,14 @@
 			<ul>
 				<li>
  					@if (isset($advert))
-					{!! Form::open(['route' => ['dashboard.advert.destroy', $advert->id], 'method' => 'DELETE']) !!}
-					<input type="name" name="txtPlaylistName" placeholder="Advert name...."
-								 value="{{ $searchItem or '' }}"/>
-					<button id="btnUp" type="button" disabled>Up</button>
- 					<button id="btnDown" type="button" disabled>Down</button>
-					<button type="button" name="btnNewPage" onclick="location.href='{{ URL::route('dashboard.advert.{adID}.page.create', $advert->id) }}';">+ New Page</button>
-					<button name="btnDeleteAdvert" type="submit">Delete Ad</button>
-					{!! Form::close() !!}
+						{!! Form::open(['route' => 'dashboard.advert.process', 'method' => 'POST']) !!}
+						<input type="name" name="txtPlaylistName" placeholder="Advert name...."
+									 value="{{ $searchItem or '' }}"/>
+						<button id="btnUp" type="button" disabled>Up</button>
+	 					<button id="btnDown" type="button" disabled>Down</button>
+						<button type="button" name="btnNewPage" onclick="location.href='{{ URL::route('dashboard.advert.{adID}.page.create', $advert->id) }}';">+ New Page</button>
+						<button name="btnDeleteAdvert" type="submit">Delete Ad</button>
+						{!! Form::close() !!}
 					@endif
 				</li>
 			</ul>
