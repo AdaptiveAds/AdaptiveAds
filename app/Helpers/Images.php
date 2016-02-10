06e50a95c164ca971fda5c7770c6d81215102abe
Josh Preece <joshpreece@outlook.com>
2016-02-10 18:17:13 +0000

    Comment update

--- app/Helpers/Images.php
+++ app/Helpers/Images.php
@@ -5,8 +5,9 @@ namespace App\Helpers;
 class Images extends Helper {
 
   /**
-    * Takes an image upload, gives it a timestamp and moves it to advert_images in the public folder
-    * @param \Illuminate\Http\Request\Input $input
+    * Takes an image upload, gives it a timestamp and moves it to an output path in the public folder
+    * @param \Illuminate\Http\Request\Input $input File to process
+    * @param string $outputPath Path to move file to in the public folder
     * @return string | null
     */
   public static function processImage($input, $outputPath) {
