6e67044c025dab5081980b221c2329137d673769
Josh Preece <joshpreece@outlook.com>
2016-02-24 22:38:03 +0000

    Removed process and delete button

--- resources/views/pages/playlistEditor.blade.php
+++ resources/views/pages/playlistEditor.blade.php
@@ -18,26 +18,19 @@
 			<ul>
 				<li>
  					@if (isset($playlist))
-						{!! Form::open(['route' => 'dashboard.playlist.process', 'method' => 'POST']) !!}
-								<input type="name" name="txtAdvertName" placeholder="Advert name...."
-											 value="{{ $searchItem or '' }}"/>
-								<button id="btnUp" type="button" disabled>Up</button>
-			 					<button id="btnDown" type="button" disabled>Down</button>
-								<button type="button" name="btnNewPage" onclick="location.href='';">+ New Page</button>
-							{!! Form::close() !!}
-							{!! Form::open(['route' => ['dashboard.advert.select', $playlist->id], 'method' => 'POST']) !!}
-								<button type="submit" name="btnAddAdvert">Add Advert</button>
-							{!! Form::close() !!}
+						<button id="btnUp" type="button" disabled>Up</button>
+	 					<button id="btnDown" type="button" disabled>Down</button>
+						<button type="button" name="btnNewPage" onclick="location.href='';">+ New Page</button>
 
-							{!! Form:: open(['route' => ['dashboard.advert.removeMode', $playlist->id], 'method' => 'POST']) !!}
-								<button type="submit" name="btnRemoveAdvert">Remove Mode</button>
-							{!! Form::close() !!}
+						{!! Form::open(['route' => ['dashboard.advert.select', $playlist->id], 'method' => 'POST']) !!}
+							<button type="submit" name="btnAddAdvert">Add Advert</button>
+						{!! Form::close() !!}
 
-							<button type="button">Edit Timings</button>
+						{!! Form:: open(['route' => ['dashboard.advert.removeMode', $playlist->id], 'method' => 'POST']) !!}
+							<button type="submit" name="btnRemoveAdvert">Remove Mode</button>
+						{!! Form::close() !!}
 
-							{!! Form::open(['route' => ['dashboard.playlist.destroy', $playlist->id], 'method' => 'DELETE']) !!}
-								<button type="submit" name="btnDeletePlaylist">Delete Playlist</button>
-							{!! Form::close() !!}
+						<button type="button">Edit Timings</button>
 					@endif
 				</li>
 			</ul>
