4459a2d4060977af39f508fb23da4a4ee6879572
Josh Preece <joshpreece@outlook.com>
2016-02-24 13:49:51 +0000

    Auth | Route change and error messages
    Auth route changed to /login and /register for simplicity and error
    messages now displayed on login failure

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,13 +21,13 @@ Route::get('contact', 'ContactController@index');
 Route::get('team', 'TeamController@index');
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
 
 // Landing page
 Route::get('dashboard', ['middleware' => 'auth', 'uses' => 'DashboardController@index']);
