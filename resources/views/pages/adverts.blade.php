a2a794eb997faab904d2e599bd3b20efae457baf
Josh Preece <joshpreece@outlook.com>
2016-02-25 13:55:56 +0000

    Removed un-needed code

--- resources/views/pages/adverts.blade.php
+++ resources/views/pages/adverts.blade.php
@@ -5,25 +5,6 @@
 @include('objects/modal_advert', array('object' => 'Advert',
 																			'heading' => 'Create New Advert',
 																			'allowed_departments' => $allowed_departments))
-@if (isset($selectedPlaylist))
-
-	<script>
-		$('document').ready(function() {
-			AdvertAssign.token = "{{ csrf_token() }}";
-	@if (isset($deleteMode))
-			AdvertAssign.action = "/dashboard/playlist/{{ $selectedPlaylist }}/remove";
-			AdvertAssign.redirectPath = "/dashboard/playlist/{{ $selectedPlaylist }}/edit";
-	@else
-			AdvertAssign.action = "/dashboard/playlist/{{ $selectedPlaylist }}/add";
-	@endif
-			AdvertAssign.redirectPath = "/dashboard/playlist/{{ $selectedPlaylist }}/edit";
-			AdvertAssign.playlist = {{ $selectedPlaylist or 1 }};
-			SelectManager.multi = true;
-			SelectManager.register_eventhandlers();
-			AdvertAssign.register_eventhandlers();
-		});
-	</script>
-@endif
 
 <script>
 	$('document').ready(function() {
@@ -44,16 +25,12 @@
 					<label name="lblDepartment">Department:</label>
  					@include('objects/dropdown_departments', array('allowed_departments' => $allowed_departments))
 
-					@if (isset($selectedPlaylist))
-						<button name="btnAddAdvert" type="button">Add</button>
-					@else
-						<a href="#AdvertModal" data-displayCreateModal="true"
-																	 data-modalObject="Advert"
-																	 data-modalMethod="POST"
-																	 data-modalRoute="{{ URL::route('dashboard.advert.store') }}">
-							<button type="button" name="btnAddAdvert">Add</button>
-						</a>
-					@endif
+					<a href="#AdvertModal" data-displayCreateModal="true"
+																 data-modalObject="Advert"
+																 data-modalMethod="POST"
+																 data-modalRoute="{{ URL::route('dashboard.advert.store') }}">
+						<button type="button" name="btnAddAdvert">Add</button>
+					</a>
 
 					<button name="btnFindAdvert" type="submit" >Find</button>
 					<button name="btnFindAll" type="submit" >Find All</button>
@@ -63,11 +40,7 @@
 	</div>
 
 	<div class="row">
-		@if (isset($selectedPlaylist))
-			@include('objects/listAdverts', array('selectable' => true))
-		@else
-			@include('objects/listAdverts', array('selectable' => false, 'editMode' => true))
-		@endif
+		@include('objects/listAdverts', array('selectable' => false, 'editMode' => true))
 	</div>
 </div>
 @endsection
