9cba5bf15b041b0702e70279c944530fe0126582
bootFairy <whelan.kane@gmail.com>
2016-05-06 17:05:46 +0100

    Updating Template Structure and Serve CSS

--- resources/views/templateDefault.blade.php
+++ resources/views/templateDefault.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<title>Playlist: {{ $screen->playlist->name }} </title>
 	<link rel="stylesheet" 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<!--link rel="stylesheet" 	href="{{ URL::asset('css/template.css') }}" type="text/css"-->
+	<link rel="stylesheet" 	href="{{ URL::asset('css/server.css') }}" type="text/css">
 	<script src="{{ URL::asset('js/jquery-2.1.4.js') }}"></script>
 	<script src="{{ URL::asset('js/modules.js') }}"></script>
 	<script src="{{ URL::asset('js/pages.js') }}"></script>
