40e121cc4bfe969b98d2d857c1aeab9c24238fea
Josh Preece <joshpreece@outlook.com>
2016-05-09 18:39:08 +0100

    Fixed pagedata issues

--- resources/views/templates/templateStructure.blade.php
+++ resources/views/templates/templateStructure.blade.php
@@ -15,19 +15,27 @@
 	<div id="header"><h1 name="pageName">{{ $pageData->heading or 'New Page'}}</h1></div>
   <div class="row">
     	<div id="serve_image">
-				@if (isset($pageData->image_path) AND $pageData->image_path != '')
-						<img src="/advert_images/{{ $pageData->image_path }}" title="" alt=""/>
-				@else
-						@if (isset($pageData->video_path) AND $pageData->video_path != '')
-							<video autoplay loop>
-								<source src="/advert_videos/{{$pageData->video_path}}" type="video/mp4">
-								<source src="/advert_videos/{{$pageData->video_path}}" type="video/ogg">
-								Your browser does not support the provided codec types.
-							</video>
-						@else
-							<img src="/images/image_placeholder.png" title="" alt=""/>
-						@endif
-				@endif
+        <video autoplay loop>
+          @if (isset($pageData))
+            @if (isset($pageData->image_path) AND $pageData->image_path != '')
+  						<img src="/advert_images/{{ $pageData->image_path }}" title="" alt=""/>
+  				@else
+  						@if (isset($pageData->video_path) AND $pageData->video_path != '')
+  							<video autoplay loop>
+  								<source src="/advert_videos/{{$pageData->video_path}}" type="video/mp4">
+  								<source src="/advert_videos/{{$pageData->video_path}}" type="video/ogg">
+  								Your browser does not support the provided codec types.
+  							</video>
+  						@else
+  							<img src="/images/image_placeholder.png" title="" alt=""/>
+  						@endif
+  				@endif
+
+            <source src="/advert_videos/video_placeholder.mp4" type="video/mp4">
+          @endif
+          Your browser does not support the provided codec types.
+        </video>
+
 			</div>
 
 			<div id="serve_text">
@@ -37,9 +45,6 @@
   <div class="clear"></div>
 </div>
 <div id="footer">
-	<ul>
-			<li>Term time hours: 8.30am – midnight</li>
-			<li>Term time weekends – Sat & Sun,  11am – 6pm</li>
-	</ul>
+
 	<div class="clear"></div>
 </div>
