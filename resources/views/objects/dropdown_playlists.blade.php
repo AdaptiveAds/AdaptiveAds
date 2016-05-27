4e6adf745eb8009093a55eca2741fff66f93ba14
Josh Preece <joshpreece@outlook.com>
2016-05-27 15:28:18 +0100

    Add default select message

--- resources/views/objects/dropdown_playlists.blade.php
+++ resources/views/objects/dropdown_playlists.blade.php
@@ -1,4 +1,5 @@
 <select name="drpPlaylists">
+  <option selected>Select a Playlist</option>
   @foreach ($playlists as $playlist)
     <option value="{{$playlist->id}}">{{$playlist->name}}</option>
   @endforeach
