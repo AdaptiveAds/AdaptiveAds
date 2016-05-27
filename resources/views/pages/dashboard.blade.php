d0d667c9000fb8392643fd3300637feefc9e6d9c
bootFairy <whelan.kane@gmail.com>
2016-05-27 14:52:30 +0100

    Resolve error on chrome #top

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -13,16 +13,7 @@
 	<div class="row">
 		<div class="options">
 			<h4>Options</h4>
-
 				<ul>
-					<li name="liPlaylist">
-						<a href="{{ URL::to('dashboard/playlist')}}">
-							<i class="fa fa-list-ul">
-							<!-- temp name ofc -->
-								<span>Playlists</span>
-							</i>
-						</a>
-					</li>
 					<li name="liPlaylistModal">
 						<a href="#PlaylistModal">
 							<i class="fa fa-list-ul">
@@ -31,12 +22,11 @@
 							</i>
 						</a>
 					</li>
-
-					<li name="liAdvert">
-						<a href="{{ URL::to('dashboard/advert')}}">
+					<li name="liPlaylist">
+						<a href="{{ URL::to('dashboard/playlist')}}">
 							<i class="fa fa-list-ul">
 							<!-- temp name ofc -->
-								<span>Adverts</span>
+								<span>Playlists</span>
 							</i>
 						</a>
 					</li>
@@ -45,6 +35,14 @@
 							<i class="fa fa-list-ul">
 							<!-- temp name ofc -->
 								<span>Create Advert</span>
+							</i>
+						</a>
+					</li>
+					<li name="liAdvert">
+						<a href="{{ URL::to('dashboard/advert')}}">
+							<i class="fa fa-list-ul">
+							<!-- temp name ofc -->
+								<span>Adverts</span>
 							</i>
 						</a>
 					</li>
@@ -59,10 +57,17 @@
 			<h4>Admin Settings</h4>
 
 				<ul>
-					<li name="liLocations">
-						<a href="{{ URL::to('dashboard/settings/locations/')}}">
-							<i class="fa fa-map-marker">
-								<span>Locations</span>
+					<li name="liUsers">
+						<a href="{{ URL::to('dashboard/settings/users')}}">
+							<i class="fa fa-users">
+								<span>Users</span>
+							</i>
+						</a>
+					</li>
+					<li name="liPrivileges">
+						<a href="{{ URL::to('dashboard/settings/privileges')}}">
+							<i class="fa fa-key">
+								<span>Privileges</span>
 							</i>
 						</a>
 					</li>
@@ -73,24 +78,18 @@
 							</i>
 						</a>
 					</li>
+					<li name="liLocations">
+						<a href="{{ URL::to('dashboard/settings/locations/')}}">
+							<i class="fa fa-map-marker">
+								<span>Locations</span>
+							</i>
+						</a>
+					</li>
+
 					<li name="liScreens">
 						<a href="{{ URL::to('dashboard/settings/screens')}}">
 							<i class="fa fa-desktop">
 								<span>Screens</span>
-							</i>
-						</a>
-					</li>
-					<li name="liUsers">
-						<a href="{{ URL::to('dashboard/settings/users')}}">
-							<i class="fa fa-users">
-								<span>Users</span>
-							</i>
-						</a>
-					</li>
-					<li name="liTemplates">
-						<a href="{{ URL::to('dashboard/settings/templates')}}">
-							<i class="fa fa-navicon">
-								<span>Page Templates</span>
 							</i>
 						</a>
 					</li>
@@ -101,10 +100,10 @@
 							</i>
 						</a>
 					</li>
-					<li name="liPrivileges">
-						<a href="{{ URL::to('dashboard/settings/privileges')}}">
-							<i class="fa fa-key">
-								<span>Privileges</span>
+					<li name="liTemplates">
+						<a href="{{ URL::to('dashboard/settings/templates')}}">
+							<i class="fa fa-navicon">
+								<span>Page Templates</span>
 							</i>
 						</a>
 					</li>
